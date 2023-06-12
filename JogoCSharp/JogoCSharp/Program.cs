9db54eea933ec407772e800bfb1a0e10c3dd39f9
innyti <116384655+innyti@users.noreply.github.com>
2023-06-12 13:48:03 -0300

    Update Program.cs

--- JogoCSharp/JogoCSharp/Program.cs
+++ JogoCSharp/JogoCSharp/Program.cs
@@ -1,9 +1,9 @@
-﻿//Não queremos manter um número fixo como número secreto sempre, então vamos criar uma forma de gerar um número pseudo-aleatório:
+﻿//Não queria manter um número fixo como número secreto sempre, então criei uma forma de gerar um número pseudo-aleatório:
 
 Random aleatorio = new Random();
 int numeroSecreto = aleatorio.Next(1, 101);
 
-//Agora, desenvolvendo a lógica do jogo, podemos usar o seguinte código:
+//Agora, desenvolvendo a lógica do jogo, o seguinte código:
 
 do
 {
@@ -26,7 +26,7 @@ do
 
 } while (true); 
 
-//Caso o chute seja igual ao número secreto, vamos exibir a mensagem indicando que o jogo acabou:
+//Caso o chute seja igual ao número secreto, ira exibir a mensagem indicando que o jogo acabou:
 
 Console.WriteLine("O jogo acabou. Você acertou o número secreto!"); 
 
